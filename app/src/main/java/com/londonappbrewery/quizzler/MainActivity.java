3d6478f29292a1a5e48c72b4c5609cc6ebdb16e4
vitezslavjaros <demosek@gmail.com>
2018-12-11 23:28:18 +0100

    napojení na db otázek

--- app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
@@ -5,33 +5,37 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    // TODO: Declare constants here
+    // TODO: Deklarace konstant
 
 
-    // TODO: Declare member variables here:
+    // TODO: Deklarace member proměnných:
     Button mTrueButton;
     Button mFalseButton;
+    TextView mQuestionTextView;
+    int mIndex;  //defaultni hodnota je 0
+    int mQuestion;
 
-    // TODO: Uncomment to create question bank
-//    private TrueFalse[] mQuestionBank = new TrueFalse[] {
-//            new TrueFalse(R.string.question_1, true),
-//            new TrueFalse(R.string.question_2, true),
-//            new TrueFalse(R.string.question_3, true),
-//            new TrueFalse(R.string.question_4, true),
-//            new TrueFalse(R.string.question_5, true),
-//            new TrueFalse(R.string.question_6, false),
-//            new TrueFalse(R.string.question_7, true),
-//            new TrueFalse(R.string.question_8, false),
-//            new TrueFalse(R.string.question_9, true),
-//            new TrueFalse(R.string.question_10, true),
-//            new TrueFalse(R.string.question_11, false),
-//            new TrueFalse(R.string.question_12, false),
-//            new TrueFalse(R.string.question_13,true)
-//    };
+    // TODO: Seznam otázek
+    private TrueFalse[] mQuestionBank = new TrueFalse[] {  // TrueFalse třída je deklarovaná v TrueFalse.java
+            new TrueFalse(R.string.question_1, true), // ze strings.xml si vytáhne text první otazky
+            new TrueFalse(R.string.question_2, true),
+            new TrueFalse(R.string.question_3, true),
+            new TrueFalse(R.string.question_4, true),
+            new TrueFalse(R.string.question_5, true),
+            new TrueFalse(R.string.question_6, false),
+            new TrueFalse(R.string.question_7, true),
+            new TrueFalse(R.string.question_8, false),
+            new TrueFalse(R.string.question_9, true),
+            new TrueFalse(R.string.question_10, true),
+            new TrueFalse(R.string.question_11, false),
+            new TrueFalse(R.string.question_12, false),
+            new TrueFalse(R.string.question_13,true)
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,16 +44,18 @@ public class MainActivity extends Activity {
 
     mTrueButton = (Button) findViewById(R.id.true_button); //konvertujeme z View do Button
     mFalseButton = findViewById(R.id.false_button);
+    mQuestionTextView = findViewById(R.id.question_text_view); //ulozi hodnotu question_text_view do mQuestionTextView
 
-    //Rozepsany onClickListener
-        View.OnClickListener myListener = new View.OnClickListener() {
+    mQuestion = mQuestionBank[mIndex].getmQuestionID(); // vrátí text otázky, getmQ... definované v TF.java
+    mQuestionTextView.setText(mQuestion);  // vloží text do textového pole mQTV
+
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.d("Quizzler","Tlačítko zmáčknuto."); // zobrazení v logu
                 Toast.makeText(getApplicationContext(),"Tlačítko TRUE zmáčknuto!",Toast.LENGTH_SHORT).show();
             }
-        };
-        mTrueButton.setOnClickListener(myListener);
+        });
 
      // Zkrácený zápis(sloučené) pro levé tlačítko
 
